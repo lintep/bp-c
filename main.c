65e20e28d1501678d61f7d81b24fa53437fc308f
rahmani_saeed <s.rahmani.ce@gmail.com>
2024-12-30 10:43:32 +0330

    My first commit

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=8;
+
+    int k=6;
+    if(k<9)
+    {
+        int b=7;
+        printf("I)%d\tb=%d",k,b);
+    }
+    if(k<5);
+    {
+        int a=7;
+        printf("II) %d\ta=%d",k,a);
+    }
+    printf("\na=%d",a);
+
+}
+
+
+//int main2()
+//{
+//
+////    int x=100;
+////    int x=100;
+////    int y=0;
+////
+////
+////    int all_c=40;
+////    int all_co=26;
+////    int all_inco=10;
+////
+////    //40   100
+////    //1    ?
+////    float one_q_ef= 100.0/all_c;
+////    printf("%f",one_q_ef*(all_co-all_inco/3.0));
+////
+////    printf("\n\nx=%d\ny=%d\n\n",x,y);
+////    printf("Hi. Hello world!\n");
+//
+////    int a=2;
+////    int b=4;
+////    int c=-3;
+////    int x=98;
+////    int y1;
+////    int y2;
+////
+////
+////    y1 = a*x + b;
+////    y2 = a*x^2 + b*x + c;
+////
+////    printf("\nx=%d",x);
+////    printf("\ny1=%d",y1);
+////    printf("\ny2=%d",y2);
+//
+//    int i;
+//    for (i=0; i<255; i++){
+//        printf("\n%d    %c",i,i);
+//    }
+//    return 0;
+//}
